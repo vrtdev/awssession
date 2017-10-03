4ebdb15b0cdcf321c179be085ff22d648b13d590
Stefan - Zipkid - Goethals <stefan.goethals@vrt.be>
2017-10-03 07:07:58 +0200

    Example use file

---
+++ exe/awssession-use.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'awssession'
+
+require 'aws-sdk-ssm'
+require 'aws_config'
+require 'pp'
+
+profile_name = 'vrt-dpc-sandbox-admin'
+profile = AWSConfig[profile_name]
+profile['name'] = profile_name
+
+awssession = AwsSession.new(profile: profile)
+awssession.start
+
+ssm = Aws::SSM::Client.new(credentials: awssession.credentials)
+
+ssm.describe_parameters.parameters.each do |p|
+  puts "Parameter Name: #{p.name}."
+end
+
+puts 'Get Parameter by Path, 2 results'
+pp ssm.get_parameters_by_path(
+  path: '/',
+  recursive: true,
+  max_results: 2
+)
